74efc0354677c9fb6d37dd2bf6155a57504a0ad4
fguardiola <fernando.guardiola.888@gmail.com>
2015-06-02 12:12:22 +0100

    Set the function to test
    Now we have the function to test isolated as a unit (unit testing) in a
    module

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express=require("express");
 var app=express();
+var getPalette=require("./lib/getPalette");
 
 
 app.set('view engine','jade');
 
+
+
 app.get("/",function(req,res){
-	res.render("index",{palette:["#cc77ff","#ff5512","#75d709"]});
+	res.render("index",{palette:getPalette()});
 });
 
 app.listen(84);
