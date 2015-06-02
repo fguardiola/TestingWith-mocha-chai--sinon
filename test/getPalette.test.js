c30c131d026d31bff039afba73c691030c9db84e
fguardiola <fernando.guardiola.888@gmail.com>
2015-06-02 15:27:19 +0100

    Dependency injection
    * Overwrite data collection fake data
    * Pass in a collection to control data, control context.
    * Dependency injection is the way we control our testing in our code

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -3,11 +3,18 @@ var assert=require("assert");//library to do assertions
 describe("getPalette",function(){
 	
 	it("should throw an error if the result is not an array",function(){
-		assert.throws(getPalette,/is not an array/);
+		function fetch(){
+			return "not array"
+		}
+
+		assert.throws(function(){
+			getPalette(fetch);
+		},/is not an array/);
 	});
 
-	it("should return an array with 3 items",function(){
-		var palette=getPalette();
+	it("should return an array with 3 items by default",function(){
+		var palette=getPalette();//no parameters so is gonna execute the normal getData function. That way we can pass the two tests
+	    
 	    assert(Array.isArray(palette),"did not return an array");
 	    assert.equal(palette.length,3,"did not return 3 items");
 	})
