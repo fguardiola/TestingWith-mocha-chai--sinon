c028658e996b28d183997f6095e5791bc4a9d238
fguardiola <fernando.guardiola.888@gmail.com>
2015-06-02 16:15:19 +0100

    Asynchronous testing
    How to test asynchronous functions

--- test/hex2rgb.test.js
+++ test/hex2rgb.test.js
@@ -2,16 +2,23 @@ var hex2rgb=require("../lib/hex2rgb");//our isolated function to be tested
 var assert=require("assert");//library to do assertions
 
 describe("hex2rgb",function(){
-	it("should throw an error if the value is not a hex code",function(){
- 		assert.throws(function(){
- 			hex2rgb("blue");
- 		},/Invalid hexadecimal string/);//we need to pass aparameter need to have anonymous function to run thge function we are testing inside 
+	it("should return an error if the value is not a hex code",function(done){
+ 		
+ 			hex2rgb("blue",function(error,result){
+ 				assert(error);
+ 				done();
+ 			});
+ 		
 	});
 
-	it("should return a correctly converted rgb value",function(){
+	it("should return a correctly converted rgb value",function(done){
 		//call function with a correct hex
-		var rgb=hex2rgb("#fff");
-		assert.deepEqual(rgb,[255,255,255]);//if you are comparing arrays or objects use deepEqual instead of equal
+		var rgb=hex2rgb("#fff",function(error,result){
+			assert.strictEqual(error,null);
+			assert.deepEqual(result,[255,255,255]);//if you are comparing arrays or objects use deepEqual instead of equal
+			done();
+		});
 		
+
 	});
 });
