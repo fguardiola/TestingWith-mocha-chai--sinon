feddcf8b8e212b2a5151c5bb08bb4c63a378b026
fguardiola <fernando.guardiola.888@gmail.com>
2015-06-03 13:01:12 +0100

    Fixtures
    * Another way to get around mocking data without overwriting config
    files (bad idea)

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -1,40 +1,18 @@
 var getPalette=require("../lib/getPalette");//our isolated function to be tested
 var assert=require("assert");//library to do assertions
-var fs=require("fs");//file system read and write config file
-var configPath=process.cwd()+"/config.json";
 
+var fakeDataPath=process.cwd()+"/test/fixtures/config-palette-non-array.json";
+//console.log("fakeDataPath:"+fakeDataPath);
 describe("getPalette",function(){
-	//afect what configuration file contains in order to mock out the data
-	function writeConfig(configObj,callback){
-		//console.log("Content to overwrite"+JSON.stringify(configObj));
-		fs.writeFile(configPath,JSON.stringify(configObj),callback);
 
-	}
-
-	var config={};
-	//before it runs any of the tests
-	before(function(done){
-		fs.readFile(configPath,function(err,result){
-			config=JSON.parse(result.toString());
-			done();
-		})
-		
-	});
-	afterEach(function(done){
-		writeConfig(config,done);
-	})
-
-	//after each one rewrite the config file back
-
-	it("should throw an error if the result is not an array",function(done){
-		writeConfig({palette:"string"},function(err){
-			//read the file after writing with data we control
-			
-			assert.throws(getPalette,/is not an array/);
+	it.only("should throw an error if the result is not an array",function(done){
+			assert.throws(function(){
+				getPalette(fakeDataPath)}
+				,/is not an array/);
 			done();
 		});
 		
-	});
+
 
 	it("should return an array with 3 items by default",function(){
 		var palette=getPalette();//no parameters so is gonna execute the normal getData function. That way we can pass the two tests
@@ -43,4 +21,3 @@ describe("getPalette",function(){
 	});
 	
 });
-
